8b5d65d52f69d8b988cecf12697fadf91ebdb757
Ling Xiao Lu <yakumoyukari17@hotmail.com>
2021-04-24 13:06:22 -0400

    add Y rotation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float moveTarget;
-    float moveAxis;
-    public float moveSpeed = 0.1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        moveAxis = 0f;
-    }
+	float moveTarget;
+	float moveAxis;
+	public float moveSpeed = 0.1f;
+	float targetRotationY;
+	float rotationY;
+	void Start()
+	{
+		moveAxis = 0f;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveAxis = Input.GetAxis("Move");
-        Vector3 diff = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
-        moveTarget = Mathf.Atan2(diff.y, diff.x);
-        transform.eulerAngles = new Vector3(0,0, moveTarget * Mathf.Rad2Deg);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		moveAxis = Input.GetAxis("Move");
+		Vector3 diff = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+		moveTarget = Mathf.Atan2(diff.y, diff.x);
+		if (Mathf.Abs(moveTarget) > Mathf.PI / 2)
+		{
+			targetRotationY = 180;
+			rotationY = Mathf.Lerp(rotationY, targetRotationY, 0.1f);
+			transform.eulerAngles = new Vector3(0, rotationY, 180 - moveTarget * Mathf.Rad2Deg);
+		} else
+		{
+			targetRotationY = 0;
+			rotationY = Mathf.Lerp(rotationY, targetRotationY, 0.1f);
+			transform.eulerAngles = new Vector3(0, rotationY, moveTarget * Mathf.Rad2Deg);
+		}
+	}
 
-    private void FixedUpdate() {
-        if (moveAxis > 0){
-            Transform parent = transform.parent;
-            parent.position = new Vector3(parent.position.x + Mathf.Cos(moveTarget) * moveAxis * moveSpeed, parent.position.y + Mathf.Sin(moveTarget) * moveAxis * moveSpeed, parent.position.z);
-        }
-    }
+	private void FixedUpdate()
+	{
+		if (moveAxis > 0)
+		{
+			Transform parent = transform.parent;
+			parent.position = new Vector3(parent.position.x + Mathf.Cos(moveTarget) * moveAxis * moveSpeed, parent.position.y + Mathf.Sin(moveTarget) * moveAxis * moveSpeed, parent.position.z);
+		}
+	}
 }
